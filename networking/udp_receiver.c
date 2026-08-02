c5758e9d8f8c2efda32d83424a8caaef8e7af6cc
KedarChandulur <kedarchandulur@gmail.com>
2026-08-02 16:18:34 +0530

    refactor: switched the udp_receiver and udp_sender behavior as they were acting in the opposite way.

--- networking/udp_receiver.c
+++ networking/udp_receiver.c
@@ -23,6 +23,8 @@
 
 // Below are the required for UDP
 
+// int bind(int sockfd, struct sockaddr *my_addr, int addrlen);
+
 // int sendto(int sockfd, const void *msg, int len, unsigned int flags,
 //            const struct sockaddr *to, socklen_t tolen);
 
@@ -32,27 +34,25 @@
 
 // Below are for TCP
 
-// int connect(int sockfd, struct sockaddr *serv_addr, int addrlen);
+// int listen(int sockfd, int backlog);
 
-// int send(int sockfd, const void *msg, int len, int flags);
-
-// int recv(int sockfd, void *buf, int len, int flags);
+// int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 
 
 int main()
 {
-    printf("\nudp_receiver init\n");
+    printf("udp_sender init\n");
 
     //const char* ipaddr = NULL; // need to change this.
     const char* ipaddr = "127.0.0.1"; // loopback addr.
     const char* port = "8080";
-    
+
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC; // Either IPv4 or IPv6
     //hints.ai_flags = AI_PASSIVE; // By using the AI_PASSIVE flag, I’m telling the program to bind to the IP of the host i am running on
     hints.ai_socktype = SOCK_DGRAM; // UDP Datagram socket
-    
+
     struct addrinfo* res = NULL;
 
     const int status = getaddrinfo(ipaddr, port, &hints, &res);
@@ -60,8 +60,8 @@ int main()
     // Error with getaddrinfo.
     if(status != 0)
     {
-        printf("\nError: getaddrinfo() failed! error status: %s", gai_strerror(status));
-        exit(EXIT_FAILURE);
+        printf("Error: getaddrinfo() failed! status: %s", gai_strerror(status));
+        return EXIT_FAILURE;
     }
 
     const int domain = res->ai_family; // Family of socket, IPv4 or IPv6.
@@ -76,54 +76,38 @@ int main()
         //printf("\nError: socket() creation failed! error status: %s", strerror(errno));
         perror("\nError: socket() creation failed");
 
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
 
-    const struct sockaddr* server_addr = (struct sockaddr*)res->ai_addr; // Destination Server address.
-    const socklen_t server_addr_len = res->ai_addrlen; // Length of server address struct
+    struct sockaddr* server_addr = (struct sockaddr*)res->ai_addr; // This Server address.
+    socklen_t server_addr_len = res->ai_addrlen; // Length of server address struct
 
-    // const int connectstatus = connect(sockfd, server_addr, server_addr_len);
+    const int bindstatus = bind(sockfd, server_addr, server_addr_len);
 
-    // if(connectstatus < 0)
-    // {
-    //     printf("\nError: connect() failed! error status: %s", strerror(errno));
-    //     //perror("\nError: connect() failed");
-
-    //     exit(EXIT_FAILURE);
-    // }
-
-    const char* message = "Hello, UDP Server!";
-    const int message_len = strlen(message);
-    const int bytes_sent = sendto(sockfd, message, message_len, 0, server_addr, server_addr_len);
-
-    if(bytes_sent != message_len)
+    // Error with binding
+    if(bindstatus < 0)
     {
-        printf("\nError: receiver send() failed!");
-    }
-    else
-    {
-        printf("\nSuccess: receiver send() succeeded!");
-    }
+        printf("\nError: bind() failed! error status: %s", strerror(errno));
+        //perror("\nError: bind() failed");
 
-    struct sockaddr* receiver_addr;
-    socklen_t* receiver_addr_len;
-
+        return EXIT_FAILURE;
+    }
+    
     char buffer[1024];
-
-    const int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, receiver_addr, receiver_addr_len);
+    const int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, server_addr, &server_addr_len);
     
     if (bytes_received < 0)
     {
-        printf("\nError: receiver recv() failed!");
+        printf("\nError: Failed to receive the data from server! error status: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     buffer[bytes_received] = '\0';
-    printf("\nreceiver Received: %s\n", buffer);
+    printf("\nMessage from server arrived: %s\n", buffer);
 
     close(sockfd);
 
     freeaddrinfo(res); // freeing the linked list.
 
-    exit(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
 }
